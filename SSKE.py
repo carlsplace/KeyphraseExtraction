40131a0a6dab9dfc0ce7a676b20f98ac73033ed3
Chang YC <carlsplace@live.cn>
2016-08-08 00:01:35 +0800

    通用omega和phi试验

--- SSKE.py
+++ SSKE.py
@@ -312,7 +312,7 @@ def create_B(node_list, gold):
                 b[neg] = 0
     return np.matrix(B)
 
-def rank_doc(file_path, file_name, alpha=0.5, d=0.85, step_size=0.1, epsilon=0.001, max_iter=1000):
+def train_doc(file_path, file_name, alpha=0.5, d=0.85, step_size=0.1, epsilon=0.001, max_iter=1000):
     file_text = readfile(file_path, file_name)
     tagged_tokens = get_tagged_tokens(file_text)
     filtered_text = get_filtered_text(tagged_tokens)
@@ -348,17 +348,13 @@ def rank_doc(file_path, file_name, alpha=0.5, d=0.85, step_size=0.1, epsilon=0.0
     g_omega = calcGradientOmega(edge_features, node_list, omega, pi3, pi, alpha, d)
     g_phi = calcGradientPhi(pi3, node_features, node_list, alpha, d)
     
-    # print(g_pi)
-    # print(g_omega)
-    # print(g_phi)
+    pi = updateVar(pi, g_pi, step_size)
+    omega = updateVar(omega, g_omega, step_size)
+    phi = updateVar(phi, g_phi, step_size)
 
     e = 1
     iteration = 0
     while  e > epsilon and iteration < max_iter and all(a >= 0 for a in phi) and all(b >= 0 for b in omega) and all(c >= 0 for c in pi):
-        pi = updateVar(pi, g_pi, step_size)
-        omega = updateVar(omega, g_omega, step_size)
-        phi = updateVar(phi, g_phi, step_size)
-
         g_pi = calcGradientPi(pi3, P, B, mu, alpha, d)
         g_omega = calcGradientOmega(edge_features, node_list, omega, pi3, pi, alpha, d)
         g_phi = calcGradientPhi(pi3, node_features, node_list, alpha, d)
@@ -373,8 +369,14 @@ def rank_doc(file_path, file_name, alpha=0.5, d=0.85, step_size=0.1, epsilon=0.0
         G0 = G1
         iteration += 1
         # print(iteration)
+        pi = updateVar(pi, g_pi, step_size)
+        omega = updateVar(omega, g_omega, step_size)
+        phi = updateVar(phi, g_phi, step_size)
     if iteration > max_iter:
         print("Over Max Iteration, iteration =", iteration)
+    pi = updateVar(pi, g_pi, -step_size)
+    omega = updateVar(omega, g_omega, -step_size)
+    phi = updateVar(phi, g_phi, -step_size)
     return pi.T.tolist()[0], omega.T.tolist()[0], phi.T.tolist()[0], node_list#, graph, filtered_text, P0, P
 
 def top_n_words(pi, node_list, n=15):
@@ -394,6 +396,27 @@ def top_n_words(pi, node_list, n=15):
 #         keywords.append(node_list[pi.index(score)])
 #     return keywords
 
+def pagerank_doc(file_path, file_name, omega, phi, d=0.85):
+    file_text = readfile(file_path, file_name)
+    tagged_tokens = get_tagged_tokens(file_text)
+    filtered_text = get_filtered_text(tagged_tokens)
+    edge_and_freq = get_edge_freq(filtered_text)
+    edge_features = add_lev_distance(edge_and_freq)#edge_freq_lev
+    edge_weight = calc_edge_weight(edge_features, omega)
+    graph = build_graph(edge_weight)
+    node_list = list(graph.node)
+
+    if 'KDD' in file_path:
+        raw_node_features = readfile('./data', 'KDD_node_features')
+    else:
+        raw_node_features = readfile('./data', 'WWW_node_features')
+    node_features = read_node_features(node_list, raw_node_features, file_name)
+    node_weight = calc_node_weight(node_features, phi)
+
+    pr = nx.pagerank(graph, alpha=d, personalization=node_weight)
+
+    return pr
+    
 starttime = datetime.datetime.now()
 
 ACCEPTED_TAGS = ['NN', 'NNS', 'NNP', 'NNPS', 'JJ']
@@ -407,14 +430,29 @@ raw_node_f = readfile('./data', 'KDD_node_features')
 #         file_name_list.append(file_name[1:])
 # write_file(str(file_name_list), './data', 'KDD_filelist')
 file_name_list = readfile('./data', 'KDD_runable').split(',')
+
+# to_file = ''
+# for file_name in file_name_list:
+#     # print(file_name, '......begin......\n')
+#     pi, omega, phi, node_list = train_doc(file_path, file_name, alpha=0.5)
+#     top_n = top_n_words(pi, node_list, n=10)
+#     gold = readfile('./data/KDD/gold', file_name)
+#     count = 0
+#     for word in top_n:
+#         if word in gold:
+#             count += 1
+#     prcs = count/len(gold.split())
+#     to_file = to_file + file_name + ',omega,' + str(omega) + ',phi,' + str(phi) + ',precision,' + str(prcs) + '\n'
+#     # print(file_name, '......end......\n')
+# write_file(to_file, './data/KDD/omega_phi', 'omegaphi-a0.5-top10.csv')
+
+omega = np.asmatrix([0.5, 0.5]).T
+phi = np.asmatrix([0.25, 0.24, 0.04, 0.25, 0.22]).T
+
 precision = ''
-psum = 0
 for file_name in file_name_list:
-    # print(file_name, '......begin......\n')
-    pi, omega, phi, node_list = rank_doc(file_path, file_name, alpha=0.5)
-    # to_file = file_name + ',' + 'omega ,' + str(omega) + ',' + 'phi ,' + str(phi) + '\n'
-    # write_file(to_file, out_path, file_name)
-    top_n = top_n_words(pi, node_list, n=15)
+    pr = pagerank_doc(file_path, file_name, omega, phi)
+    top_n = top_n_words(list(pr.values()), list(pr.keys()), n=10)
     gold = readfile('./data/KDD/gold', file_name)
     count = 0
     for word in top_n:
@@ -422,22 +460,7 @@ for file_name in file_name_list:
             count += 1
     prcs = count/len(gold.split())
     precision = precision + file_name + ',' + str(prcs) + '\n'
-    psum += prcs
-    # print(file_name, '......end......\n')
-precision = precision + 'average,' + str(psum/25)
-write_file(precision, './data/KDD', 'precision25-n15a0.5.csv')
-print(precision)
-# paper_name = '10971362'
-# pi, omega, phi, node_list = rank_doc('./data/KDD/abstracts',paper_name)
-# top_n = top_n_words(pi, node_list)
-# print(top_n)
-
-
-# print("pi:", pi)
-# print("omega:", omega)
-# print("phi:", phi)
-# print(node_list)
-
+write_file(precision, './data/KDD', 'rank_precision-top10.csv')
 # tokens = nltk.word_tokenize(text)
 # tagged_tokens = nltk.pos_tag(tokens)
 # tagged_tokens = get_tagged_tokens(file_text)
